f899fef89964e446cafdc1c5ca3271c66b0d97f1
Will Foran <willforan@gmail.com>
2021-09-29 11:57:28 -0400

    🎨 utils: inc and dec buttons for dev cards

--- src/landscape/utils.cljs
+++ src/landscape/utils.cljs
@@ -1,13 +1,14 @@
 (ns landscape.utils
    (:require [sablono.core :as sab :include-macros true :refer-macros [html]]))
 
-(defn inc-time [state-atom]
-  (swap! state-atom update :time-cur  #(+ 100 %)))
+(defn inc-time [state-atom inc]
+  (swap! state-atom update :time-cur  #(+ inc %)))
 (defn wrap-state [state-atom chunk]
   (html [:div.wrapped 
          chunk
          [:div
-          [:button {:onClick (fn [_] (inc-time state-atom))} "+100ms"]
+          [:button {:onClick (fn [_] (inc-time state-atom -100))} "-100ms"]
+          [:button {:onClick (fn [_] (inc-time state-atom 100))} "+100ms"]
           (str " time: " (:time-cur @state-atom))]]))
 
 (defn now [] (.getTime (js/Date.)))
