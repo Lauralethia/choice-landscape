7a980eb2c2927b942ef1a296bbf2166bfff2a2cc
Will Foran <willforan@gmail.com>
2021-10-15 14:44:19 -0400

    💄 move board down 100px so :up well isn't in water when far
    TODO: still zoom and fullscreen?

--- src/landscape/settings.cljs
+++ src/landscape/settings.cljs
@@ -2,12 +2,14 @@
 
 (def BOARD "bounds and properties of the background board"
   {:center-x 250
-   :bottom-y 400
-   :avatar-home {:x 250 :y 400}
+   :bottom-y 500
+   :avatar-home {:x 250 :y 500}
    :step-sizes [70 150 50 25 25 25 25] ; currently only first 2 are used 20211005
    :wait-time 500 ; TODO: force movement at this speed? currently used only by well animation
    })
 
+; not used everywhere!
+; TODO: replace all hard coded numbers
 (def KEYCODES {:left  37
               :up     38
               :right  39
