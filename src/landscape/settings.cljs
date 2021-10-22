3881096fc2bca1f3876d2f23db735980323fe19b
Will Foran <willforan@gmail.com>
2021-10-22 13:27:55 -0400

    💄 move wells+avatar up 100px b/c top well is shorter
    home is now 400px. but progress bar is below that

--- src/landscape/settings.cljs
+++ src/landscape/settings.cljs
@@ -2,8 +2,8 @@
 
 (def BOARD "bounds and properties of the background board"
   {:center-x 250
-   :bottom-y 500
-   :avatar-home {:x 250 :y 500}
+   :bottom-y 400
+   :avatar-home {:x 250 :y 400}
    :step-sizes [70 150 50 25 25 25 25] ; currently only first 2 are used 20211005
    :top-scale .66 ; perspective makes up seem farther away. scale move and disntace by this much
    :bar-pos {:x 50 :y 500} ; where to position the progress bar
