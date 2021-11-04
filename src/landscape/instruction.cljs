8bbee09ece3ef894233a4b53e3f7cceedbe7c6d9
Will Foran <willforan@gmail.com>
2021-11-04 16:17:22 -0400

    💄 instructions: high->full

--- src/landscape/instruction.cljs
+++ src/landscape/instruction.cljs
@@ -89,7 +89,7 @@
              (assoc-in state [:water :active-at] time-cur))
     :stop (fn [{:keys [water time-cur] :as state}]
             (assoc-in state [:water] (water/water-state-fresh)))} 
-   {:text (fn[_] "And get the water as high as possible!")
+   {:text (fn[_] "And get the water as full as possible!")
     :pos (fn[_] {:x 50 :y 250})
     :start (fn[{:keys [water time-cur] :as state}]
              (assoc-in state [:water :level] 100))
