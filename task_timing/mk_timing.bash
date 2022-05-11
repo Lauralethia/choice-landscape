fca308f0eeb75e4bd4348a26d7b617844793126e
WillForan <willforan@gmail.com>
2022-05-10 21:26:56 -0400

    ✨ timing: add_glt for additional contrasts

--- task_timing/mk_timing.bash
+++ task_timing/mk_timing.bash
@@ -45,7 +45,7 @@ trial_counts(){
   total_catch=$(( $good_catch + $nogood_catch))
   # quick look. might have added or lost a trial
   new_total=$(( $good + $good_catch + $nogood + $nogood_catch ))
-  [[ $total -ne $new_total ]] && warn "WARNING: wanted $total trials, but using $new_total" 
+  [[ $total -ne $new_total ]] && warn "WARNING: wanted $total trials, but using $new_total"
   echo $good $good_catch $nogood $nogood_catch $total_nocatch $total_catch
 }
 # lisp macro a la tcl: unsafe string interpolation
@@ -73,14 +73,34 @@ parse_decon(){
     }' -- -name="$1"
 }
 
+add_glt(){
+   # add glts to decon command file
+   # needs already have e.g. '-numglt 3' and '-x1D'
+   # input is file and then any number of contrast+label pairs like
+   #   add_glt decon.tsch 'a -b' 'a-b' 'c +.5*d' 'c_halfd'
+   cmd_file="$1";shift
+   nglt=$(grep -Po '(?<=-num_glt )\d+' $cmd_file||echo "")
+   [ -z "$nglt" ] && warn "ERROR: no -num_glt in '$cmd_file'" && exit 1
+   newn=$nglt
+   glts=""
+   while [ $# -gt 0 ]; do
+      let newn++
+      glts="$glts -gltsym 'SYM: $1' -glt_label $newn $2"
+      shift 2
+   done
+
+   sed -e "s/-num_glt $nglt/-num_glt $newn/" -i $cmd_file
+   sed -e "s;-x1D;$glts -x1D;" -i $cmd_file
+}
+
 mktiming(){
-  total="$1"; shift         # expecting 
+  total="$1"; shift         # expecting
   total_runtime="$1"; shift # something like 540
   eval $MACRO_TRIAL_COUNTS # set good good_catch ...
-  
+
   # second arg should be seed. can use random if not provided
   # TODO: max $RANDOM vs max make_random_timing seed
-  [ $# -ne 0 ] && seed="$1" || seed="$RANDOM" 
+  [ $# -ne 0 ] && seed="$1" || seed="$RANDOM"
 
   outdir=${total_runtime}s/${PREFIX}_${total}_$seed
   [ -d $outdir ] && warn "# have $outdir dir, skipping" && return 0
@@ -124,11 +144,14 @@ mktiming(){
      -seed $seed                        \
      -prefix stimes > mktiming.log
 
-  #TODO:
-  # use sed to inject extra GLTs for eg. 
-  #  good + nogood - g_fbk + ng+fbk
-  # would need to update -numglt
-  # and add in
+  #TODO: better contrasts?
+
+  add_glt decon.tcsh \
+     'good +nogood -g_fbk -ng_fbk' 'choice-fbk' \
+     '.6*good -.3*g_catch'    'good_and_catch' \
+     '.6*nogood -.3*ng_catch' 'nogood_and_catch' \
+     'nogood +nogood +g_catch +ng_catch' 'choice'
+
   tcsh decon.tcsh > decon.log
   parse_decon "${PREFIX}-${total_runtime}-${total}-${seed}" < decon.log > stddevtests.tsv
   1d_tool.py -cormat_cutoff 0.1 -show_cormat_warnings -infile X.stimes.xmat.1D > timing_cor.txt
@@ -154,7 +177,7 @@ fi
 #   bats ./mk_timing.bash --verbose-run
 ####
 if  [[ "$(caller)" =~ /bats.*/preprocessing.bash ]]; then
-function trialcount_test { #@test 
+function trialcount_test { #@test
   local total=12
   eval $MACRO_TRIAL_COUNTS
   warn "'$good' '$nogood' '$good_catch' '$nogood_catch'"
@@ -163,7 +186,7 @@ function trialcount_test { #@test
   [ $good_catch -eq 3 ]
   [ $nogood_catch -eq 1 ]
 }
-function parse_test { #@test 
+function parse_test { #@test
 
  output="$(cat <<HERE| parse_decon XXX
 Stimulus: ng_zzz_c
@@ -173,10 +196,19 @@ General Linear Test: good_c-nogood_c
   LC[0] norm. std. dev. =   5.6284
 HERE
 )"
- 
+
  warn "$output"
- [[ $output =~ ng_zz_c_h ]]
+ [[ $output =~ ng_zzz_c_h ]]
  [[ $output =~ good_c-nogood_c_LC ]]
  [[ $output =~ 3.3897 ]]
+}
+function add_glt_test { #@test
+   f=/tmp/glttestfile
+   echo "-num_glt 4 -x1D " > $f
+   run add_glt $f 'a +b' 'a_P_b' 'a -b' 'a-b'
+   warn "file: '$(cat $f)'"
+   grep "\-num_glt 6" $f
+   grep "SYM: a +b" $f
+   grep "\-glt_label 6 a-b" $f
 }
 fi
