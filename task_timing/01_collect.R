aa85900f9e351191f770f97d1627665737800d9e
WillForan <willforan@gmail.com>
2022-05-10 20:43:40 -0400

    🚧 timing: add prefix, documentation, and organize by total dur

--- task_timing/01_collect.R
+++ task_timing/01_collect.R
@@ -3,7 +3,7 @@
 # 20220510WF - init
 #  collect all std dev norm tests. lowest is best
 suppressPackageStartupMessages(library(dplyr))
-d <- Sys.glob('out/*/stddevtests.tsv') %>% lapply(read.table,header=T) %>% bind_rows
+d <- Sys.glob('out/*/*/stddevtests.tsv') %>% lapply(read.table,header=T) %>% bind_rows
 write.table(d, 'std_dev_tests.tsv', row.names=F)
 
 
--- task_timing/mk_timing.bash
+++ task_timing/mk_timing.bash
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 #
 # generate timings for MR task with decaying ITI and catch trials
-# TODO: consider variable ISI "zzz"
+# TODO:
+#  * consider variable ISI "ZZZ_TIME" (see comment in var def)
+#  * sed in more GLTs to deconvolve
 #
 # 20220510WF - init
 #
@@ -11,17 +13,25 @@ warn(){ echo "$@" >&2; }
 # hard coded timings from piloting
 # NB: RT probably different in MR comared to amazon turk
 
+TR=1.3
 MEAN_RT=0.580     # but can be up to 1.5
-ZZZ_TIME=1        # hardcode as one second always? could use decay?
 WALK_TIME=0.534   # always take the same time to walk to feedback
 FBK_TIME=1        # always 1 second of feedback
+ZZZ_TIME=1        # hardcode as one second always
+#ZZZ_TIME="0.5 1 3 dist=decay" between .5 and 3 with mean of 1. exp distributed
+
+# save directory prefix incase we update e.g. GLT SYMS or timings
+# but want to hold onto old
+# will already be organized by total runtime and include total_trials
+#   outdir=${total_runtime}s/${PREFIX}_${total}_$seed
+PREFIX=v1
 
 # average trial time is 2.1 seconds
 # mean iti should be ~3 (exp disp)
 # want maybe 100 trials at 5s per trial+iti = 500s total
 # 500 + 100s if never responded
 
-TR=1.3
+
 
 trial_counts(){
   # 2/3 of trials have the good choice, 1/3 without the good choice
@@ -38,12 +48,11 @@ trial_counts(){
   [[ $total -ne $new_total ]] && warn "WARNING: wanted $total trials, but using $new_total" 
   echo $good $good_catch $nogood $nogood_catch $total_nocatch $total_catch
 }
-
-# lisp macro a la tcl: via unsafe string interpolation
+# lisp macro a la tcl: unsafe string interpolation
 # set variables usinng trial_counts where it's eval'ed
 # here so we dont have to keep changing the read. just update (likely append) variable names
+# should match output of trail_counts function
 MACRO_TRIAL_COUNTS='read good good_catch nogood nogood_catch total_nocatch total_catch <<< "$(trial_counts $total)"'
-
 
 
 # from https://github.com/LabNeuroCogDevel/slipstask/tree/master/timing
@@ -73,39 +82,41 @@ mktiming(){
   # TODO: max $RANDOM vs max make_random_timing seed
   [ $# -ne 0 ] && seed="$1" || seed="$RANDOM" 
 
-  [ -d $seed ] && warn "# have $seed dir, skipping" && return 0
-  mkdir -p $seed
-  (cd $seed
+  outdir=${total_runtime}s/${PREFIX}_${total}_$seed
+  [ -d $outdir ] && warn "# have $outdir dir, skipping" && return 0
+  mkdir -p $outdir
+  (cd $outdir
   make_random_timing.py \
      -tr $TR \
      -num_runs 1 -run_time $total_runtime        \
      -pre_stim_rest 6 -post_stim_rest 6           \
      -rand_post_stim_rest no                      \
-     -add_timing_class choice_w_good  "$MEAN_RT"  \
-     -add_timing_class choice_wo_good "$MEAN_RT"  \
-     -add_timing_class walk           "$WALK_TIME"\
-     -add_timing_class feedback       "$FBK_TIME" \
-     -add_timing_class zzz            $ZZZ_TIME \
+     -add_timing_class s_choice_w_good  "$MEAN_RT"  \
+     -add_timing_class s_choice_wo_good "$MEAN_RT"  \
+     -add_timing_class s_walk           "$WALK_TIME"\
+     -add_timing_class s_feedback       "$FBK_TIME" \
+     -add_timing_class s_zzz            $ZZZ_TIME \
      \
      -add_timing_class nobreak 0 0 0 dist=INSTANT \
      -add_timing_class iti 0 -1 8     \
      \
-     -add_stim_class good_c          "$good"          choice_w_good  nobreak \
-     -add_stim_class nogood_c        "$nogood"        choice_wo_good nobreak \
-     -add_stim_class g_fbk_c         "$good"          feedback nobreak       \
-     -add_stim_class g_walk_c        "$good"          walk nobreak           \
-     -add_stim_class ng_fbk_c        "$nogood"        feedback iti \
-     -add_stim_class ng_walk_c       "$nogood"        walk nobreak           \
-     -add_stim_class g_catch_c       "$good_catch"    choice_w_good  nobreak     \
-     -add_stim_class ng_catch_c      "$nogood_catch"  choice_wo_good nobreak     \
-     -add_stim_class g_zzz_c         "$good_catch"    zzz iti     \
-     -add_stim_class ng_zzz_c        "$nogood_catch"  zzz iti     \
+     -add_stim_class good          "$good"          s_choice_w_good  nobreak \
+     -add_stim_class g_fbk         "$good"          s_feedback nobreak       \
+     -add_stim_class g_walk        "$good"          s_walk nobreak           \
+     -add_stim_class g_catch       "$good_catch"    s_choice_w_good  nobreak     \
+     -add_stim_class g_zzz         "$good_catch"    s_zzz iti     \
+     \
+     -add_stim_class nogood        "$nogood"        s_choice_wo_good nobreak \
+     -add_stim_class ng_fbk        "$nogood"        s_feedback iti \
+     -add_stim_class ng_walk       "$nogood"        s_walk nobreak           \
+     -add_stim_class ng_catch      "$nogood_catch"  s_choice_wo_good nobreak     \
+     -add_stim_class ng_zzz        "$nogood_catch"  s_zzz iti     \
      `:          g ng gfb gwk ngf ngw gc ngc gz ngz -- not sure this works with ordered_stim`\
      -max_consec 3  3   0   0   0   0  2   2  0   0 \
-     -ordered_stimuli good_c g_walk_c g_fbk_c \
-     -ordered_stimuli nogood_c ng_walk_c ng_fbk_c \
-     -ordered_stimuli g_catch_c  g_zzz_c  \
-     -ordered_stimuli ng_catch_c ng_zzz_c \
+     -ordered_stimuli good g_walk g_fbk \
+     -ordered_stimuli nogood ng_walk ng_fbk \
+     -ordered_stimuli g_catch  g_zzz  \
+     -ordered_stimuli ng_catch ng_zzz \
      -show_timing_stats                 \
      -make_3dd_contrasts                \
      -write_event_list events.txt \
@@ -113,8 +124,13 @@ mktiming(){
      -seed $seed                        \
      -prefix stimes > mktiming.log
 
+  #TODO:
+  # use sed to inject extra GLTs for eg. 
+  #  good + nogood - g_fbk + ng+fbk
+  # would need to update -numglt
+  # and add in
   tcsh decon.tcsh > decon.log
-  parse_decon $seed < decon.log > stddevtests.tsv
+  parse_decon "${PREFIX}-${total_runtime}-${total}-${seed}" < decon.log > stddevtests.tsv
   1d_tool.py -cormat_cutoff 0.1 -show_cormat_warnings -infile X.stimes.xmat.1D > timing_cor.txt
 )
 }
