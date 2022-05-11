7c9d1eae17aaa300f3e606bd8fcd9aaeb5bdcea9
WillForan <willforan@gmail.com>
2022-05-10 20:25:39 -0400

    🚧 timing: collect h/lc tests

---
+++ task_timing/01_collect.R
@@ -0,0 +1,10 @@
+#!/usr/bin/env Rscript
+
+# 20220510WF - init
+#  collect all std dev norm tests. lowest is best
+suppressPackageStartupMessages(library(dplyr))
+d <- Sys.glob('out/*/stddevtests.tsv') %>% lapply(read.table,header=T) %>% bind_rows
+write.table(d, 'std_dev_tests.tsv', row.names=F)
+
+
+
