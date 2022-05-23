2856c2364a8f83f664afbd21da79b5e46fb8adac
WillForan <willforan@gmail.com>
2022-05-23 16:18:24 -0400

    🚧 mr: best timing into edn task layout

--- task_timing/show_times.bash
+++ task_timing/show_times.bash
@@ -1,1 +1,4 @@
-sed '1,3d;$d;s/(\|)//g' out/500s/v1_102_31234/events.txt | awk '/good|nogood|catch/{e=$2}; !/ 0.00$/{print e, $5} END{print e, 500-$3}'
+#!/usr/bin/env bash
+[ $# -eq 1 ] && fname="$1" || fname=out/500s/v1_102_31234/events.txt 
+sed '1,3d;$d;s/(\|)//g' $fname |
+    awk '/good|nogood|catch/{e=$2}; !/ 0.00$/{print e, $5} END{print e, 500-$3}'
