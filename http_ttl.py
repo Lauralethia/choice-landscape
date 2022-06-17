e5aa8ade5e71d11b4ab5d6ef21c76fc3c9ba2f56
WillForan <willforan@gmail.com>
2022-06-17 15:53:18 -0400

    🚧 templates for LPT, DAQ, and RTBox
    totally untested. but RTBox code pulled from previous session w/iEEG hardware

--- http_ttl.py
+++ http_ttl.py
@@ -3,10 +3,13 @@
 http server to send ttls
 task javascript -> POST /123 -> hardware ttl
 
+also polls for button boxes, sends ttl on push
+and forwards as keypush
+
 unknowns:
  * CORS issue if using heroku?
  * terrible timing?
- * async? might want to be watching for button pushes here too?
+ * key pushes on windows?
 """
 
 
@@ -30,6 +33,41 @@ class Hardware():
     """
     def send(self, ttl):
         print(f"sending {ttl} @ {datetime.datetime.now()}")
+
+    def wait_and_zero(self):
+        # TODO: more percise sleeping psychopy.core.wait(.005) or psychtoolbox.WaitSecs(.005)
+        time.sleep(.005)  # wait 5ms and send zero. not precise enough?
+        self.send(0)
+        #  if we're lucky setData zero's pins that aren't used
+        # self.port.setData(0)
+
+
+# LPT + Cedrus == Loeffler EEG
+class LPT(Hardware):
+    """ set TTL on parallel port (LPT) """
+    def __init__(self, address):
+        from psychopy import parallel
+        import time
+        self.port = parallel.ParallelPort(address=address)
+
+    def send_todo(ttl):
+        self.port.setData(ttl)
+        # TODO: needed?
+        # self.wait_and_zero()
+
+
+class DAQ(Hardware):
+    def __init__():
+        import usb1208FS
+        pass
+    def send_todo(ttl):
+        usb1208FS.DOut(usb1208FS.DIO_PORTA, ttl)
+        # TODO: do we need to zero?
+        # self.wait_and_zero()
+
+
+
+### BUTTON BOXES
 
 class Cedrus():
     """ cedrus response box (RB-x40) """
@@ -63,6 +101,44 @@ class Cedrus():
                 self.dev.poll_for_response()
             response = self.dev.get_next_response()
             self.trigger(response)
+
+class RTBox():
+    def __init__(self, hw):
+        self.run = True
+        self.hw = hw
+        self.keys = ['1', '2', '3', '4', 'S', 'L', '5', 'A']
+
+        sys.path.insert(0,'/home/abel/luna_habit/RTBox_py') # or PYTHONPATH=~/luna_habit/RTBox/python
+        import RTBox
+        box = RTBox.RTBox()
+        box.enable('light')
+        box.info()
+        box.threshold(4)
+        print(f"using threshold {box.threshold()} (1-4)")
+        box.close()
+
+        _ser = box._ser
+        _ser.open()
+        _ser.write(b'x') # simple mode: 1 byte per event
+        _ser.write(bytearray([101, 0b111101])) # e, all except release
+        _ser.read(1)
+        self._ser = _ser
+
+    def trigger(self, index):
+        response = self.keys[index]
+        self.hw.send(f"{response}")
+
+
+    async def watch(self):
+        while self.run:
+            b = bin(ord(self._ser.read(1)))[::-1]
+            # do we actually want to ignore?
+            # we can work directly on the bits? -- maybe just pull the first
+            # dont want to miss a PD trigger b/c a button was pushed
+            # also... how fast is counting and finding?
+            if b.count('1')==1: # ignore if +1 bits set
+                self.trigger(b.find('1'))
+
 
 class KB():
     def __init__(self):
