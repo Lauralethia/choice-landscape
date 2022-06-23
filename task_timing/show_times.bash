534328aa21b6b053a5e4a0a361a565bdfebc4a78
WillForan <willforan@gmail.com>
2022-06-23 10:34:41 -0400

    ✨ timing: funcs.R; check rep catch trials; seeds 6156,23263,1599

--- task_timing/show_times.bash
+++ task_timing/show_times.bash
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-[ $# -eq 1 ] && fname="$1" || fname=out/500s/v1_102_31234/events.txt 
-maxtime=$(grep -Po '\d+(?=s)' <<< $fname)
-sed '1,3d;$d;s/(\|)//g' $fname |
-    awk -v mt="$maxtime" '/good|nogood|catch/{e=$2}; !/ 0.00$/{print e, $5} END{print e, mt-$3}'
+[ $# -eq 0 ] && echo "USAGE: $0 out/500s/v1_102_31234/events.txt" >&2 && exit 1
+for fname in "$@"; do
+  # needed to get the last iti
+  maxtime=$(grep -Po '\d+(?=s)' <<< "$fname" | sed 1q)
+  #echo "# fname: $fname maxtime: $maxtime" >&2
+  sed '1,3d;$d;s/(\|)//g' "$fname" |
+      awk -v mt="$maxtime" '/good|nogood|catch/{e=$2}; !/ 0.00$/{print e, $5} END{print e, mt-$3}'
+done
