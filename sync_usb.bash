1c85c0e99567c5bb7af87435edfec5c358f7dbb0
WillForan <willforan@gmail.com>
2022-07-28 16:00:41 -0400

    🎨 cleaner sync

--- sync_usb.bash
+++ sync_usb.bash
@@ -2,4 +2,12 @@
 # windows doesn't use symlinks (by default)
 # make git/version control difficult on usb (to transfer to offline windows PC)
 # just sync new changes (-u for updated only -- dont change things newer on usb)
-rsync --size-only -urvLhi ../choice-landscape/ /mnt/usb/task/choice-landscape/ --exclude .git --exclude results --exclude target --exclude .cljs_node_repl --exclude .cpcache "$@"
+rsync --size-only -urvLhi ../choice-landscape/ /mnt/usb/task/choice-landscape/ \
+   --exclude .git \
+   --exclude results \
+   --exclude target \
+   --exclude .cljs_node_repl \
+   --exclude .cpcache \
+   --exclude 'clojure-tools*' \
+   --exclude '*.sqlite3' \
+   "$@"
