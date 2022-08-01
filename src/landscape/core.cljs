aa2d67606b0c504980926f63877e7873aa6799a9
WillForan <willforan@gmail.com>
2022-08-01 16:04:00 -0400

    🎨 core: lispy-mode induced spacing change
    own comment b/c cannot figure out why git makes a big deal from lines
    that look unchanged

--- src/landscape/core.cljs
+++ src/landscape/core.cljs
@@ -40,40 +40,38 @@
   ;; TODO: might want to get fixed timing
   ;;       look into javascript extern (and supply run or CB) to pull from edn/file
   (let[best-side (best-well-side left-best)
-        prob-low (get-in  @current-settings [:prob :low] ) ; initially 20
-        prob-mid (get-in  @current-settings [:prob :mid] ) ; initially 50
-        prob-high (get-in @current-settings [:prob :high] ) ; originally 100, then 90 (20211104)
+       prob-low (get-in  @current-settings [:prob :low] ) ; initially 20
+       prob-mid (get-in  @current-settings [:prob :mid] ) ; initially 50
+       prob-high (get-in @current-settings [:prob :high] ) ; originally 100, then 90 (20211104)
        prob-deval-other (get-in @current-settings [:prob :devalue-good :other]) ;likely 75 or 100 depending on settings
-       n-devalue-good-trials (get-in @current-settings [:nTrials :devalue-good])
-        
-       ]
+       n-devalue-good-trials (get-in @current-settings [:nTrials :devalue-good])]
     (vec (concat
-       ;; first set of 24*(3 lowhigh + 3 highlow + 3 lowhigh):
-       ;; initially had two close wells with infreq rewards. far is all rewards
-       ;; then version where all wells are equidistant but 2 are still meh
-       ;; finally a version with an additional block where the good well is deval
-       (->
-        ; ({:left 100 :right 30 :up 20}, {... :up 30 :right 20})
-        (timeline/side-probs prob-low prob-mid best-side)
-        ;; 20220314 maybe no reversal!?
-        (#(take (if (:reversal-block @current-settings) 2 1) %))
+        ;; first set of 24*(3 lowhigh + 3 highlow + 3 lowhigh):
+        ;; initially had two close wells with infreq rewards. far is all rewards
+        ;; then version where all wells are equidistant but 2 are still meh
+        ;; finally a version with an additional block where the good well is deval
+        (->
+         ; ({:left 100 :right 30 :up 20}, {... :up 30 :right 20})
+         (timeline/side-probs prob-low prob-mid best-side)
+         ;; 20220314 maybe no reversal!?
+         (#(take (if (:reversal-block @current-settings) 2 1) %))
 
-        ; ({:resp-each-side 24 :left 100...}, {:reps-each-side ...}
-        (timeline/add-reps-key (-> @current-settings :nTrials :pairsInBlock)) ; 24*3 trials per block
+         ; ({:resp-each-side 24 :left 100...}, {:reps-each-side ...}
+         (timeline/add-reps-key (-> @current-settings :nTrials :pairsInBlock)) ; 24*3 trials per block
 
-        ; 20211216 - remove. now only 2 blocks before deval
-        ; ({..:up 20}, {.. :up 30}, {.. :up 20})
-        ; timeline/add-head-to-tail
+         ; 20211216 - remove. now only 2 blocks before deval
+         ; ({..:up 20}, {.. :up 30}, {.. :up 20})
+         ; timeline/add-head-to-tail
 
-        vec
+         vec
 
-        ; 20211216 - remove. all equal
-        ; prev more trial for first block (19-16 *3 = 9 more)
-        ;(assoc-in [0 :reps-per-side] 19)
+         ; 20211216 - remove. all equal
+         ; prev more trial for first block (19-16 *3 = 9 more)
+         ;(assoc-in [0 :reps-per-side] 19)
         
-        ;; make it look like what well drawing funcs want
-        ((partial mapcat #'timeline/gen-prob-maps))
-        ((partial mapv #'timeline/well-trial)))
+         ;; make it look like what well drawing funcs want
+         ((partial mapcat #'timeline/gen-prob-maps))
+         ((partial mapv #'timeline/well-trial)))
 
         ;; removed 2021-12-09
         ; ;; add 4 forced trials where we cant get to the good
@@ -85,31 +83,31 @@
         ;           :reps-each-side 2
         ;           :side-best best-side}))
 
-       ;; 20211216 - one set to ensure everything is seen equally at the start
-       ;; 3 trials total: lr, lu, ru ... but random
-       (timeline/gen-wells
-        {:prob-low prob-high
-         :prob-high prob-high
-         :reps-each-side 1
-         :side-best best-side})
+        ;; 20211216 - one set to ensure everything is seen equally at the start
+        ;; 3 trials total: lr, lu, ru ... but random
+        (timeline/gen-wells
+         {:prob-low prob-high
+          :prob-high prob-high
+          :reps-each-side 1
+          :side-best best-side})
 
 
-       ;; all wells are good:  4 reps of 6 combos
-       (timeline/gen-wells
-        {:prob-low prob-high
-         :prob-high prob-high
-         :reps-each-side (max 0 (- (-> @current-settings :nTrials :devalue) 1))
-         ; 20211104 increased from 4 to 8; 8*(2 high/low swap, h=l)*(3 per perm)
-         ;; 20211216 inc to 9 (+1 above)
-         :side-best best-side})
-       ;; 20220314 - optionally add a 4th block to devalue good
-       (when (> n-devalue-good-trials 0)
-         (timeline/gen-wells
-          (get-in @current-settings [:prob :devalue-good :good])
-          {:prob-low prob-deval-other
-           :prob-high prob-deval-other
-           :reps-each-side n-devalue-good-trials
-           :side-best best-side}))))))
+        ;; all wells are good:  4 reps of 6 combos
+        (timeline/gen-wells
+         {:prob-low prob-high
+          :prob-high prob-high
+          :reps-each-side (max 0 (- (-> @current-settings :nTrials :devalue) 1))
+                                      ; 20211104 increased from 4 to 8; 8*(2 high/low swap, h=l)*(3 per perm)
+          ;; 20211216 inc to 9 (+1 above)
+          :side-best best-side})
+        ;; 20220314 - optionally add a 4th block to devalue good
+        (when (> n-devalue-good-trials 0)
+          (timeline/gen-wells
+           (get-in @current-settings [:prob :devalue-good :good])
+           {:prob-low prob-deval-other
+            :prob-high prob-deval-other
+            :reps-each-side n-devalue-good-trials
+            :side-best best-side}))))))
 
 (defn vis-class-to-body
   "style body based on visual type (desert/mountain)"
