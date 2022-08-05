fffe2ff4e0eef04740d7fad23145f6ac09cf385b
Will Foran <willforan@gmail.com>
2022-08-05 08:57:13 -0400

    🎨 mk_stim: blacken. also fix order of iti_ttl calc

--- results/LoeffEEGPhotoTiming/mk_stim.py
+++ results/LoeffEEGPhotoTiming/mk_stim.py
@@ -3,115 +3,125 @@ import re
 import mne
 import numpy as np
 import pandas as pd
+
 # plotting
 import matplotlib
-matplotlib.use('TkAgg')
+# need to set the plot type before loading plt
+matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
-plt.ion()
+
 # local extra functions and and a way to reload them
 import util
 from importlib import reload
+
+## settings
+plt.ion()
 
 # print without scientific notation
 np.set_printoptions(precision=3)
 np.set_printoptions(suppress=True)
 
 
-### files we're using
-bdf='./data/remotebtn.bdf'
-tsv='./data/remotebtn.tsv'
-#bdf='./data/pdint3.bdf'
-#tsv='./data/pdtest3.tsv'
+## files we're using
+bdf = "./data/remotebtn.bdf"
+tsv = "./data/remotebtn.tsv"
+# bdf='./data/pdint3.bdf'
+# tsv='./data/pdtest3.tsv'
 
-#bdf='./data/short_practice_stimtest.bdf'
-#tsv='./data/short.tsv'
+# bdf='./data/short_practice_stimtest.bdf'
+# tsv='./data/short.tsv'
 
 
-### read in eeg and get separate stim channel info
+## read in eeg and get separate stim channel info
 eeg = mne.io.read_raw_bdf(bdf)
-#eeg.describe() # 247808 (484.0 s) == 512Hz
+# eeg.describe() # 247808 (484.0 s) == 512Hz
 
 # events = mne.find_events(eeg, shortest_event=2)
 util.add_stim_corrected(eeg)
-events = mne.find_events(eeg,stim_channel='StatusCorrected',shortest_event=2)
+events = mne.find_events(eeg, stim_channel="StatusCorrected", shortest_event=2)
 
 
-first_iti_idx = np.where(np.bitwise_and(events[:,2] >= 10, events[:,2]<=15))[0][0]
-e_timed = events[first_iti_idx:,[0,2]].astype('float')
-first_event = e_timed[0,0]
-e_timed[:,0] = (e_timed[:,0] - first_event)/eeg.info['sfreq']
+first_iti_idx = np.where(np.bitwise_and(events[:, 2] >= 10, events[:, 2] <= 15))[0][0]
+e_timed = events[first_iti_idx:, [0, 2]].astype("float")
+first_event = e_timed[0, 0]
+e_timed[:, 0] = (e_timed[:, 0] - first_event) / eeg.info["sfreq"]
 
-t_light  = e_timed[ e_timed[:,1]==1]
-t_button = e_timed[ np.bitwise_and(e_timed[:,1]>1,e_timed[:,1]<5)]
-t_task   = e_timed[ np.bitwise_and(e_timed[:,1]>=5,e_timed[:,1]<256) ]
+t_light = e_timed[e_timed[:, 1] == 1]
+t_button = e_timed[np.bitwise_and(e_timed[:, 1] > 1, e_timed[:, 1] < 5)]
+t_task = e_timed[np.bitwise_and(e_timed[:, 1] >= 5, e_timed[:, 1] < 256)]
 
 
+## read in event data
+td = pd.read_csv(tsv, sep='\t')
+start_time = td["iti_onset"][0]
+task_onsets = (
+    td.filter(regex="onset")
+    .transform(lambda df: (df - start_time) / 1000)
+    .melt()
+    .sort_values("value")
+    .assign(name=lambda d: [re.sub("_onset", "", x) for x in d.variable])
+    .reset_index()
+)
 
-###### read in event data
-td = pd.read_csv(tsv,sep="\t")
-start_time = td['iti_onset'][0]
-task_onsets = td.filter(regex="onset").\
-              transform(lambda df: (df - start_time)/1000).\
-              melt().\
-              sort_values('value').\
-              assign(name=lambda d: [re.sub('_onset','',x) for x in d.variable]).\
-              reset_index()
+## Dataframe
 
-#### plot
-plt.close()
-plt.plot(t_light, np.repeat(1,len(t_light)), 'ro')
-         #t_button, np.repeat(2,len(t_button)), 'bs',
-         #task_onsets.value, np.repeat(0,len(task_onsets)), 'k.',
-         #t_task,   np.repeat(3,len(t_task)), 'g^')
+task_onsets.iloc["name", task_onsets.name.values == None] = "None"
+ttl_df = pd.DataFrame(
+    [util.taskttl_event_side(x) for x in t_task[:, 1]],
+    columns=["e", "sides", "picked", "score"],
+).assign(ttl=t_task[:, 1], onset=t_task[:, 0])
 
-iti_ttl = ttl_df[ttl_df.e == 'iti'].onset.values
-iti_task = task_onsets[task_onsets.name=='iti'].value.values
-iti_task_trunc = iti_task[iti_task < iti_ttl[-1] + .1]
+# None as a string so we can color later
+ttl_df.loc[ttl_df.e.values == None, "e"] = "None"
+factors = np.unique(np.concatenate([ttl_df.e.values, task_onsets.name.values])).tolist()
+
+# iti_ttl used to find/id matching PD events
+# iti_task_trunc used to draw lines on plot
+iti_ttl = ttl_df[ttl_df.e == "iti"].onset.values
+iti_task = task_onsets[task_onsets.name == "iti"].value.values
+iti_task_trunc = iti_task[iti_task < iti_ttl[-1] + 0.1]
+
+# difference between task and Stim channel
 iti_ttl - iti_task_trunc
+
+## plot
+plt.close()
+plt.plot(t_light, np.repeat(1, len(t_light)), "ro")
+# t_button, np.repeat(2,len(t_button)), 'bs',
+# task_onsets.value, np.repeat(0,len(task_onsets)), 'k.',
+# t_task,   np.repeat(3,len(t_task)), 'g^')
+
 # [0.   ,  0.023,  0.034,  0.011,  0.019,  0.031,  0.011,  0.016, -0.007]
 # line for each new trial
 for xi in iti_task_trunc:
-    plt.axvline(x=xi, color='b')
+    plt.axvline(x=xi, color="b")
 
-task_onsets.iloc['name',task_onsets.name.values == None] = 'None'
-ttl_df = pd.DataFrame([util.taskttl_event_side(x) for x in t_task[:,1]],
-                      columns=['e','sides','picked','score']).\
-        assign(ttl=t_task[:,1],
-               onset=t_task[:,0])
 
-ttl_df.loc[ttl_df.e.values == None, 'e'] = 'None'
-
-factors = np.unique(np.concatenate([ttl_df.e.values,
-                                   task_onsets.name.values])).tolist()
 cvals = [factors.index(f) for f in task_onsets.name.values]
-plt.scatter(task_onsets.value,
-            np.repeat(0,len(task_onsets)),
-            c=cvals)
+plt.scatter(task_onsets.value, np.repeat(0, len(task_onsets)), c=cvals)
 
 cvals = [factors.index(f) for f in ttl_df.e]
-plt.scatter(ttl_df.onset, np.repeat(3,len(ttl_df)), c=cvals)
-#plt.scatter(t_task[:,0], np.repeat(2.7,len(t_task)))
+plt.scatter(ttl_df.onset, np.repeat(3, len(ttl_df)), c=cvals)
+# plt.scatter(t_task[:,0], np.repeat(2.7,len(t_task)))
 
-cvals=[['blue','black','green'][int(x)-2] for x in t_button[:,1]]
-plt.scatter(t_button[:,0], np.repeat(2,len(t_button)), c=cvals)
+cvals = [["blue", "black", "green"][int(x) - 2] for x in t_button[:, 1]]
+plt.scatter(t_button[:, 0], np.repeat(2, len(t_button)), c=cvals)
 
-side_factor = ['left','up','right']
+side_factor = ["left", "up", "right"]
 
 for i in range(40):
-   plt.annotate(int(t_task[i,1]), (t_task[i,0],3), rotation=30)
+    plt.annotate(int(t_task[i, 1]), (t_task[i, 0], 3), rotation=30)
 for i in range(40):
-   plt.annotate(int(t_button[i,1]), (t_button[i,0],2))
+    plt.annotate(int(t_button[i, 1]), (t_button[i, 0], 2))
 for i in range(40):
-   plt.annotate(task_onsets.name[i], (task_onsets.value[i],0), rotation=45)
+    plt.annotate(task_onsets.name[i], (task_onsets.value[i], 0), rotation=45)
 
 plt.show()
 
 
-
-
 #### OFFSET
-pd_onset = t_light[0,0]
-i=0
+pd_onset = t_light[0, 0]
+i = 0
 pd_match = []
 iti_with_pd = iti_ttl[1:]
 for iti in iti_with_pd:
@@ -119,8 +129,8 @@ for iti in iti_with_pd:
         if pd_onset > iti:
             pd_match.append(pd_onset)
             break
-        pd_onset = t_light[i,0]
-        i = i +1
-iti_to_pd = pd_match - iti_with_pd[0:len(pd_match)] 
-np.mean(iti_to_pd)*1000 # 48.915
-np.std(iti_to_pd)*1000  #  6.009
+        pd_onset = t_light[i, 0]
+        i = i + 1
+iti_to_pd = pd_match - iti_with_pd[0 : len(pd_match)]
+np.mean(iti_to_pd) * 1000  # 48.915
+np.std(iti_to_pd) * 1000  #  6.009
--- results/LoeffEEGPhotoTiming/util.py
+++ results/LoeffEEGPhotoTiming/util.py
@@ -1,4 +1,6 @@
 import numpy as np
+import mne
+
 # see also: find_events(...,mask=2**17 -256)
 # mask=2**17 -256
 def correct_ttl(x):
@@ -6,10 +8,11 @@ def correct_ttl(x):
     >>> correct_ttl(np.array([16128],dtype='int64')) # first stim ch val
     np.array([0],dtype='int16')
     """
-    return x.astype(np.int32) - 127**2 +1
-    v = x - 16128 #  np.log2(16128+256)==14
-    v[v==65536] = 0 # 65536==2**16
+    return x.astype(np.int32) - 127**2 + 1
+    v = x - 16128  #  np.log2(16128+256)==14
+    v[v == 65536] = 0  # 65536==2**16
     return v
+
 
 def ttl_side_info(side_info):
     ##                 10   11  13
@@ -17,89 +20,93 @@ def ttl_side_info(side_info):
     ## left+up=3       13   14
     ## left+right=4    15       17
     ## up+right=5           16  18
-    combo=('left+up','left+right','up+right')
-    aval_sides=None
-    picked=None
+    combo = ("left+up", "left+right", "up+right")
+    aval_sides = None
+    picked = None
     if side_info < 10:
         try:
-            aval_sides=combo[side_info%10 -1]
-        except: pass
+            aval_sides = combo[side_info % 10 - 1]
+        except:
+            pass
     elif side_info == 13:
-        aval_sides='left+up'
-        picked='left'
+        aval_sides = "left+up"
+        picked = "left"
     elif side_info == 14:
-        aval_sides='left+up'
-        picked='up'
+        aval_sides = "left+up"
+        picked = "up"
     elif side_info == 15:
-        aval_sides='left+right'
-        picked='left'
+        aval_sides = "left+right"
+        picked = "left"
     elif side_info == 17:
-        aval_sides='left+right'
-        picked='right'
+        aval_sides = "left+right"
+        picked = "right"
     elif side_info == 16:
-        aval_sides='up+right'
-        picked='up'
+        aval_sides = "up+right"
+        picked = "up"
     elif side_info == 18:
-        aval_sides='up+right'
-        picked='right'
-    return(aval_sides, picked)
+        aval_sides = "up+right"
+        picked = "right"
+    return (aval_sides, picked)
+
 
 def aval_sides_only(ttl):
-   ttl_m = ttl % 10 - 3
-   aval_sides = ['left+up','left+right','right+up' ][ttl_m]
-   return aval_sides
+    ttl_m = ttl % 10 - 3
+    aval_sides = ["left+up", "left+right", "right+up"][ttl_m]
+    return aval_sides
+
 
 def taskttl_event_side(ttl):
     # 128/129 = start/stop
     # iti chose catch timeout waiting feedback   other
     #  10    20    50     70      150      200     230
     # side sum(left=1,up=2,right=3):
-    #left+up left+right up+right
+    # left+up left+right up+right
     #      3          4        5
     # picked   left   up    right
     #           10 || 11 || 13
     # score = +10
-    ttl=int(ttl)
-    sides=('left','up','right')
-    (ename, aval_sides, picked, score) = [None]*4
+    ttl = int(ttl)
+    sides = ("left", "up", "right")
+    (ename, aval_sides, picked, score) = [None] * 4
     if ttl == 128:
-        ename='start'
+        ename = "start"
     elif ttl == 129:
-        ename='stop'
-    elif ttl >= 10 and ttl < 20: # 10-15
-        ename='iti'
+        ename = "stop"
+    elif ttl >= 10 and ttl < 20:  # 10-15
+        ename = "iti"
         aval_sides = aval_sides_only(ttl)
-    elif ttl < 50: # 20 - 50
-        ename =  'chose'
+    elif ttl < 50:  # 20 - 50
+        ename = "chose"
         aval_sides = aval_sides_only(ttl)
         ttl_m = ttl % 10 - 3
-        aval_sides = ['left','up','right' ][ttl_m]
+        aval_sides = ["left", "up", "right"][ttl_m]
     elif ttl < 70:
-        ename='catch'
+        ename = "catch"
         aval_sides, picked = ttl_side_info(ttl - 50)
     elif ttl < 150:
-        ename =  'timeout'
+        ename = "timeout"
         aval_sides, picked = ttl_side_info(ttl - 70)
     elif ttl < 200:
-        ename='waiting'
+        ename = "waiting"
         aval_sides, picked = ttl_side_info(ttl - 150)
-        #aval_sides, picked = ttl_side_info(ttl - 150)
+        # aval_sides, picked = ttl_side_info(ttl - 150)
     elif ttl < 230:
-        ename='feedback'
+        ename = "feedback"
         rm = 200
         if ttl > 220:
             rm = rm + 10
             score = True
         else:
             score = False
-        aval_sides, picked = ttl_side_info(ttl-rm)
+        aval_sides, picked = ttl_side_info(ttl - rm)
 
     return (ename, aval_sides, picked, score)
 
+
 def add_stim_corrected(raw):
     raw.load_data()
-    stim_raw = raw.pick_channels(['Status']).get_data()
-    info = mne.create_info(['StatusCorrected'], raw.info['sfreq'], ['stim'])
+    stim_raw = raw.pick_channels(["Status"]).get_data()
+    info = mne.create_info(["StatusCorrected"], raw.info["sfreq"], ["stim"])
     stim_vals = correct_ttl(stim_raw[0]).reshape(stim_raw.shape)
     stim = mne.io.RawArray(stim_vals, info)
     raw.add_channels([stim], force_update_info=True)
