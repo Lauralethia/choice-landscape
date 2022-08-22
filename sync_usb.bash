16e79e5c819351512126a88cf5e8949792430186
WillForan <willforan@gmail.com>
2022-08-22 12:12:06 -0400

    ✨ sync_usb.bash: exclude json files

--- sync_usb.bash
+++ sync_usb.bash
@@ -10,11 +10,12 @@ rsync --size-only -urvLhi ../choice-landscape/ /mnt/usb/task/choice-landscape/ \
    --exclude .cpcache \
    --exclude 'clojure-tools*' \
    --exclude '*.sqlite3' \
+   --exclude '*.json' \
    --exclude '*.cljs.cache.json'\
    --exclude 'out/landscape' \
    --exclude 'out/cljs*' \
-   --exclude 'out/*/' \
    --include 'out/imgs/' \
    --include 'out/audio/' \
    --include 'out/extra/' \
+   --exclude 'out/*/' \
    "$@"
