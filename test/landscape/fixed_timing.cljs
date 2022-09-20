b25b9d087b929475ab7b6ee8480fa9f39e0dea67
Will Foran <willforan@gmail.com>
2022-09-19 20:19:27 -0400

    🧪 fixed-timing not equally distributed!?
    102/3 (sides) -> 34/3 (blocks) -> 11.333
    can't be totally equal

--- test/landscape/fixed_timing.cljs
+++ test/landscape/fixed_timing.cljs
@@ -5,3 +5,17 @@
 (deftest ideal-trial-time-test
   (is (= [1 5 8]
          (map :iti-ideal-end (utils/iti-ideal-end 2 [{:iti-dur 1} {:iti-dur 2} {}] 1)))))
+
+(defn cnt-reps [vec]  (reduce (fn[x y] (update x y inc)) {} vec))
+(defn get-choices [timing]
+  (map (fn[t] (filter (fn[s] (get-in t [s :open]))
+                      [:left :up :right]))
+       timing))
+(defn get-lens [tid]
+  (-> ft/trials tid get-choices cnt-reps))
+(deftest check-sides-represented
+  (let [idea [34 34 34]]
+    (is (= (vals (get-lens :mrA1)) [34 34 34]))
+    (is (= (vals (get-lens :mrA2)) [34 34 34]))
+    (is (= (vals (get-lens :mrB1)) [34 34 34]))
+    (is (= (vals (get-lens :mrB2)) [34 34 34]))))
