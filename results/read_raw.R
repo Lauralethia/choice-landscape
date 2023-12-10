03fab6b69555278ff040c9ba65c664c97e9efb01
WillForan <willforan@gmail.com>
2023-12-10 12:35:40 -0500

    ♻️ read_raw.R from habit_plots.R

---
+++ results/read_raw.R
@@ -0,0 +1,48 @@
+read_raw <- function(fname="lab.data.tsv") {
+
+  # exclude testing runs (IDs with our initials, or 'x')
+  BAD_IDS <- c("WWF|ACP|^x$")
+  MIN_TRIALS <- 90
+
+  rawdata <- read.csv(fname, sep='\t') %>% 
+    filter(!grepl(BAD_IDS, id)) %>%
+    mutate(vdate=ifelse(is.na(vdate)|vdate=="", paste0(timepoint, " 00:00:00"), vdate),
+           vdate=lubridate::ymd_hms(vdate),
+           age = as.numeric(age),
+           survey_age = as.numeric(survey_age),
+           age=ifelse(is.na(age),survey_age,age))
+    #filter(id != 'WWF34M' & id != 'ACP34F' & id != 'AP' & id != 'FC' & ver == '20211104v4-90max_pbar_moredeval')
+    #filter(ver == '20211025v3-longertrials')
+  
+  # have various combinations of blocks
+  #   init-switch1-devalue_all_100
+  #   init-switch1-devalue_all_100-devalue_all_low
+  blockseq_df <-
+      rawdata %>% group_by(id,ver,timepoint) %>%
+      arrange(trial) %>%
+      summarise(blockseq=paste0(unique(blocktype),collapse="-"))
+  
+  # set fawWell and initHigh side: 1 (left) ,2 (up), or 3 (right)
+  farProb <- max(rawdata$up_prob) # expect 100. but might be 95
+  first_trial_wellnames <- rawdata %>% filter(trial == 1) %>% 
+      mutate(
+          # best (was at one time far, likely all same distance now) 
+          farWell = ifelse(left_prob == farProb, 1, ifelse(up_prob==farProb, 2, ifelse(right_prob==farProb, 3, NA))),
+          # side that was first high (likely of 50/20) 
+          initHigh = ifelse(left_prob == 50, 1, ifelse(up_prob==50, 2, 3))) %>%
+      select(id, farWell, initHigh)
+
+  # make sure we have enough trials to use
+  total_trials <- rawdata %>%
+       group_by(id, vdate, age=survey_age) %>%
+       summarize(ntrials = max(trial)) %>%
+       filter(ntrials > MIN_TRIALS)
+   
+   
+  perSubj <- merge(total_trials, first_trial_wellnames, by='id')
+
+  rawdata %>%
+      merge(perSubj, by=c('id','vdate')) %>%
+      left_join(blockseq_df, by=c("id","ver","timepoint")) %>%
+  add_choice_cols 
+}
